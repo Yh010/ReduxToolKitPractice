1affdb2b3da45a092f7c98c03010b3c449374ee2
Yash Hegde <yash4success@gmail.com>
2024-03-19 13:31:33 +0530

    useSelector to catch the current state of todos

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { removeTodo } from '../features/todo/todoSlice.js';
+
 
 function Todos() {
+    const todos = useSelector(state => state.todos)
   return (
     <div>Todos</div>
   )
